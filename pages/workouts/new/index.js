5d889fe851a932be6e80a02c84a39687a7c1cc59
Manuel Martinez <m.martinez45@hotmail.com>
2022-09-23 17:16:04 -0400

    added exercise to previous day feature (still buggy)

--- pages/workouts/new/index.js
+++ pages/workouts/new/index.js
@@ -7,34 +7,22 @@ import * as Styles from './NewWorkout.module.scss'
 
 function newWorkout(){
     const { user, exercises } = useAuth()
-    // const [ userWorkouts, setUserWorkouts ] = useState([])
     const [ searchTerm, setSearchTerm] = useState('')
     const [ sets, setSets ] = useState('')
     const [ reps, setReps ] = useState('')
     const [ filteredExercises, setFilteredExercises] = useState([])
-    const [ preExercises, setPreExercises ] = useState([])
+    const [ dayExercises, setDayExercises ] = useState([])
     const [ days, setDays ] = useState([])
-    const [ dayCount, setDayCount ] = useState(1)
     const [ exerciseInputFields, setExerciseInputFields ] = useState([{
         'name': '',
         'sets': '',
         'reps': ''
     }]) 
 
-    
-    // const getWorkouts = async () => {
-    //     const querySnapshot = await getDocs(collection(db, "workouts"));
-    //     querySnapshot.forEach((doc) => {
-    //         const data = doc.data()
-    //         if(doc.data().user === user.uid) setUserWorkouts(userWorkouts => [...userWorkouts, data])
-    //     })
-    // }
-
-
     const addDay = (day) => {
         addExercise()
-        setDays(days => [...days, preExercises])
-        setPreExercises([])
+        setDays(days => [...days, dayExercises])
+        setDayExercises([])
     }
 
 
@@ -54,19 +42,24 @@ function newWorkout(){
         setFilteredExercises(newFilter)
     }
 
+    const showFilterResults = () => {
+        return 
+            filteredExercises.length != 0 && ( 
+            <div className={Styles.exerciseResults}>
+                {filteredExercises.map((exercise, key) => {
+                    return <div onClick={exerciseSelect} name={exercise.name} className={Styles.dataItem} key={exercise._id}>{exercise.name}</div>
+                })}
+            </div>
+        )
+    }
+
     const exerciseSelect = (e) => {
         setSearchTerm(e.target.innerHTML)
         setFilteredExercises([])
     }
 
     const addExercise = () => {
-        // e.preventDefault()
-        // setExerciseInputFields([...exerciseInputFields, {
-        //     name: '',
-        //     sets: '',
-        //     reps: ''
-        // }])
-        setPreExercises(preExercises => [...preExercises, {
+        setDayExercises(dayExercises => [...dayExercises, {
             name: searchTerm,
             sets: sets,
             reps: reps
@@ -76,10 +69,70 @@ function newWorkout(){
         setReps('')
     }
 
-    // useEffect(() => {
-    //     getWorkouts()
-    // }, [])
+    const addDayExercise = (dayIndex) => {
+        const allDays = [...days]
+        const dayExercises = allDays[dayIndex]
+        dayExercises.push({
+            name: searchTerm,
+            sets: sets,
+            reps: reps
+        })
+        allDays.splice(dayIndex, 1, dayExercises)
+        setDays(allDays)
+    }
 
+    const handleRemove = (index) => {
+        const values = [...dayExercises]
+        values.splice(index, 1)
+        setDayExercises(values)
+    }
+
+    const handleDayExerciseRemove = (dayIndex, exerciseIndex) => {
+        const allDays = [...days]
+        const dayExercises = allDays[dayIndex]
+        dayExercises.splice(exerciseIndex, 1)
+        allDays.splice(dayIndex, 1, dayExercises)
+        setDays(allDays)
+    }
+
+    const showInputs = (dayIndex) => {
+        return exerciseInputFields.map((input, index) => (
+                    <Row key={index}>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label onClick={() => setFilteredExercises('exercise')}>Exercise: </Form.Label>
+                                <Form.Control name={'exercise' + dayIndex} placeholder="Search exercise, muscle group, equipment..." type='text' onChange={(e) => handleFilter(e)} value={searchTerm}/> 
+                                { 
+                                    filteredExercises.length != 0 && ( 
+                                    <div className={Styles.exerciseResults}>
+                                        {filteredExercises.map((exercise, key) => {
+                                            return <div onClick={exerciseSelect} name={exercise.name} className={Styles.dataItem} key={exercise._id}>{exercise.name}</div>
+                                        })}
+                                    </div>
+                                )} 
+                                </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group>
+                            <Form.Label>Sets: </Form.Label>
+                                <Form.Control 
+                                    value={sets} 
+                                    name="sets" 
+                                    type='text' 
+                                    onChange={(e) => {
+                                        isNaN(e.target.value) ? alert('Not a Number') : setSets(e.target.value)
+                                    }}/>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label>Reps: </Form.Label>
+                                <Form.Control value={reps} name="reps" type='text' onChange={(e) => setReps(e.target.value)}/>
+                            </Form.Group>
+                        </Col>
+                    </ Row>
+                ))
+            }
 
     return (
         <>
@@ -96,12 +149,13 @@ function newWorkout(){
                 </Form.Group>
 
                 <Container fluid>
-            {   days.length > 0 ? days.map((day, index) => ( 
-                <div key={index + 1}>
-                    <h5>Day:{index + 1} </h5>
+                    {/* ********************* PreExercises Pre Day ******************** */}
+            {   days.length > 0 ? days.map((day, dayIndex) => ( 
+                <div key={dayIndex + 1}>
+                    <h5>Day: {dayIndex + 1} </h5>
                     {
-                    day.map((exercise, key) => (
-                            <Row key={key}>
+                    day.map((exercise, exerciseIndex) => (
+                            <Row key={exerciseIndex}>
                                 <Col>
                                     <Form.Group>
                                         <Form.Label>Exercise: </Form.Label>
@@ -120,16 +174,26 @@ function newWorkout(){
                                         <Form.Control disabled defaultValue={exercise.reps} />
                                     </Form.Group>
                                 </Col>
+                                <Col>
+                                    <Button onClick={(e) => handleDayExerciseRemove(dayIndex, exerciseIndex)} variant='danger'> - </Button>
+                                </Col>
                             </Row>
-                ))}
-                <h5>Day: { days.length + 1}</h5>
+                            
+                            
+                            ))}
+                            {showInputs(dayIndex)}
+                <Button onClick={(e) => addDayExercise(dayIndex)}> Add Exercise </Button>
                 </ div>
-            )) : 
-            <>
-                    <h5>Day: 1</h5>
-                    {
-                    preExercises.map((exercise, key) => (
-                            <Row key={key}>
+            )) 
+            : <></>} 
+            </Container>
+            {/* **************************** Pre Exercises Same Day ***************** */}
+            <Container>
+                <h5>Day: {days.length + 1}</h5>
+                {/* <h5>Day: {days.length !== 0 ? days.length + 1 : 1 }</h5> */}
+               {
+                    dayExercises.map((exercise, index) => (
+                            <Row key={index}>
                                 <Col>
                                     <Form.Group>
                                         <Form.Label>Exercise: </Form.Label>
@@ -148,47 +212,14 @@ function newWorkout(){
                                         <Form.Control disabled defaultValue={exercise.reps} />
                                     </Form.Group>
                                 </Col>
+                                <Col>
+                                    <Button type='button' onClick={(e) => handleRemove(index)} value={index} variant='danger'> - </Button>
+                                </Col>
                             </Row>
                 ))}
-                </>
-                } 
-            </Container>
-            <Container>
-            {
-                exerciseInputFields.map((input, key) => (
-                    <Row key={key}>
-                        <Col>
-                            <Form.Group>
-                                <Form.Label onClick={() => setFilteredExercises('exercise')}>Exercise: </Form.Label>
-                                <Form.Control name="exercise" placeholder="Search exercise, muscle group, equipment..." type='text' onChange={handleFilter} value={searchTerm}/> 
-                                { 
-                                    filteredExercises.length != 0 && ( 
-                                    <div className={Styles.exerciseResults}>
-                                        {filteredExercises.map((exercise, key) => {
-                                            return <div onClick={exerciseSelect} name={exercise.name} className={Styles.dataItem} key={exercise._id}>{exercise.name}</div>
-                                        })}
-                                    </div>
-                                )} 
-                                </Form.Group>
-                        </Col>
-                        <Col>
-                            <Form.Group>
-                            <Form.Label>Sets: </Form.Label>
-                                <Form.Control value={sets} name="sets" type='text' onChange={(e) => setSets(e.target.value)}/>
-                            </Form.Group>
-                        </Col>
-                        <Col>
-                            <Form.Group>
-                                <Form.Label>Reps: </Form.Label>
-                                <Form.Control value={reps} name="reps" type='text' onChange={(e) => setReps(e.target.value)}/>
-                            </Form.Group>
-                        </Col>
-                    </ Row>
-                ))
-            }
             </Container>
 
-
+                {showInputs(days.length)}
 
                 <Button onClick={addExercise}> Add Exercise </Button>
                 <Button onClick={ addDay }>Add Day</Button>
