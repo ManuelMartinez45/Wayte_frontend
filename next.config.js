c068b997e302becb61dee1a1418197ef78b23269
Manuel Martinez <m.martinez45@hotmail.com>
2022-09-27 16:20:38 -0400

    nextConfig fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+//  @type {import('next').NextConfig} 
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// module.exports = nextConfig
-const withTM = require('next-transpile-modules')(['react-haiku']);
-module.exports = withTM({})
+module.exports = nextConfig
